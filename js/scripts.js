cb2afd4beecbd0b2f6553160eb45098f43ca33ef
Isabel Hasse and Joe McKinney <isabel@>
2017-10-23 19:43:56 -0700

    Add javascript to show animal info according to dropdown menu selection.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-  
+  $("#animals").submit(function(event){
+    event.preventDefault();
+    var animal = $("#animal").val();
+    $(".animal").hide();
+    if (animal === "1") {
+      $("#turtle").show();
+    } else if (animal === "2") {
+      $("#frog").show();
+    } else if (animal === "3") {
+      $("#octopi").show();
+    } else if (animal === "4") {
+      $("#kinkajou").show();
+    }
+  });
 });
